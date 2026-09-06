c6da98602213c44c403e031815ce2586edee5cec
TuocMia <tuocmia117@gmail.com>
2026-09-06 14:08:02 +0700

    bare-metal stm32

---
+++ led_fx/main.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+
+#define RCC_BASE    0x40021000
+#define GPIOA_BASE   0x40010800
+
+#define RCC_APB2ENR (*(volatile uint32_t *)(RCC_BASE + 0x18))
+#define GPIOA_CRL   (*(volatile uint32_t *)(GPIOA_BASE + 0x00))
+#define GPIOA_ODR   (*(volatile uint32_t *)(GPIOA_BASE + 0x0C))
+
+void delay(volatile int time)
+{
+    while (time > 0) {
+        time = time - 1;
+    }
+}
+
+int main(void) {
+    RCC_APB2ENR |= (1 << 2);        // Enable clock for GPIOA
+    GPIOA_CRL = 0x22222222;         // Output push-pull, 2MHz for PA0-7
+
+    while(1) {
+        for(int i = 0; i <= 7; i++) {
+            GPIOA_ODR = (1 << i);
+            delay(300000);
+        }
+        for(int i = 7; i > 0; i--){
+            GPIOA_ODR = (1 << (i - 1));
+            delay(300000);
+        }
+    }
+}
