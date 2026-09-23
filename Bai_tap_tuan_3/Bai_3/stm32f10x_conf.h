fd144aeb58fb8528fc4261d3e02ffd820dde6d4f
Phuc Lai <laiproduction05@gmail.com>
2026-09-24 06:53:17 +0700

    Add Bai_3

---
+++ Bai_tap_tuan_3/Bai_3/stm32f10x_conf.h
@@ -0,0 +1,13 @@
+#ifndef __STM32F10x_CONF_H
+#define __STM32F10x_CONF_H
+
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_usart.h"
+#include "stm32f10x_dma.h"
+#include "stm32f10x_exti.h"
+#include "misc.h" 
+
+#define assert_param(expr) ((void)0)
+
+#endif
